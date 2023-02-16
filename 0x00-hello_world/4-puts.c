244b87a43e6ff9ede094a84a27eb1ecc6f8ce19b
Tshepo-2002 <tshepohlwempu2002@gmail.com>
2023-02-16 15:17:32 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * main - "Programming is like building a multilingual puzzle
  * Return: 0 (Success)
- */		
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
